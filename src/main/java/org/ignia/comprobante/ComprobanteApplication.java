24a42e61dd5ec7e45ce69a3947ae391781b5ad70
ju4n1t0x <juanignaciosasia@gmail.com>
2026-08-31 22:57:03 -0300

    second commit

---
+++ src/main/java/org/ignia/comprobante/ComprobanteApplication.java
@@ -0,0 +1,13 @@
+package org.ignia.comprobante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprobanteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComprobanteApplication.class, args);
+    }
+
+}
